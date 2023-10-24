d13bbfca32147cc268b26a542cf0026a761cd882
Michele Morgigno <michelemorgigno@gmail.com>
2023-10-24 16:41:27 +0200

    fix: currentUser name and surname fetched from db instead of Keycloak

--- client/src/utils/AuthContext.js
+++ client/src/utils/AuthContext.js
@@ -1,60 +1,60 @@
-import { createContext, useState, useEffect } from "react";
+import {createContext, useState, useEffect} from "react";
 import jwtDecode from "jwt-decode";
 import ProfilesAPI from "../api/profiles/profilesApi";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-  const [currentUser, setCurrentUser] = useState({});
+    const [currentUser, setCurrentUser] = useState({});
 
-  const isTokenExpired = (token) => {
-    const decodedToken = jwtDecode(token);
-    return !!(decodedToken.exp && decodedToken.exp * 1000 < Date.now());
-  };
-
-  const logout = () => {
-    localStorage.removeItem("token");
-    setCurrentUser({});
-  };
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      const token = localStorage.getItem("token");
-
-      if (token) {
-        if (!isTokenExpired(token)) {
-          const decodedToken = jwtDecode(token);
-          const role =
-            decodedToken.resource_access["springboot-keycloak-client"].roles[0];
-          let id = null;
-          if (role === "Manager") {
-            id = await ProfilesAPI.getManager(decodedToken.email);
-          }
-          if (role === "Expert") {
-            id = await ProfilesAPI.getExpert(decodedToken.email);
-          }
-          if (role === "Client") {
-            id = await ProfilesAPI.getCustomer(decodedToken.email);
-            console.log(id);
-          }
-
-          setCurrentUser({
-            email: decodedToken.email,
-            name: decodedToken.given_name,
-            surname: decodedToken.family_name,
-            username: decodedToken.preferred_username,
-            role: decodedToken.resource_access["springboot-keycloak-client"]
-              .roles[0],
-            id: id?.id,
-          });
-        } else {
-          logout();
-        }
-      }
+    const isTokenExpired = (token) => {
+        const decodedToken = jwtDecode(token);
+        return !!(decodedToken.exp && decodedToken.exp * 1000 < Date.now());
     };
 
-    fetchToken();
-  }, []);
+    const logout = () => {
+        localStorage.removeItem("token");
+        setCurrentUser({});
+    };
 
-  return [currentUser, logout];
+    useEffect(() => {
+        const fetchToken = async () => {
+            const token = localStorage.getItem("token");
+
+            if (token) {
+                if (!isTokenExpired(token)) {
+                    const decodedToken = jwtDecode(token);
+                    const role =
+                        decodedToken.resource_access["springboot-keycloak-client"].roles[0];
+                    let db_user = null;
+                    if (role === "Manager") {
+                        db_user = await ProfilesAPI.getManager(decodedToken.email);
+                    }
+                    if (role === "Expert") {
+                        db_user = await ProfilesAPI.getExpert(decodedToken.email);
+                    }
+                    if (role === "Client") {
+                        db_user = await ProfilesAPI.getCustomer(decodedToken.email);
+                    }
+
+                    setCurrentUser({
+                        email: decodedToken.email,
+                        name: db_user?.name,
+                        surname: db_user?.surname,
+                        username: decodedToken.preferred_username,
+                        role: decodedToken.resource_access["springboot-keycloak-client"]
+                            .roles[0],
+                        id: db_user?.id,
+                        profile: db_user
+                    });
+                } else {
+                    logout();
+                }
+            }
+        };
+
+        fetchToken();
+    }, []);
+
+    return [currentUser, logout];
 };
--- client/src/views/global/Sidebar.jsx
+++ client/src/views/global/Sidebar.jsx
@@ -1,207 +1,207 @@
-import { useContext, useState } from "react";
+import {useContext, useState} from "react";
 import {
-  Sidebar as ProSidebar,
-  Menu,
-  MenuItem,
-  sidebarClasses,
-  menuClasses,
+    Sidebar as ProSidebar,
+    Menu,
+    MenuItem,
+    sidebarClasses,
+    menuClasses,
 } from "react-pro-sidebar";
-import { Avatar, Box, IconButton, Typography, useTheme } from "@mui/material";
-import { Link, useLocation } from "react-router-dom";
-import { tokens } from "../../theme";
+import {Avatar, Box, IconButton, Typography, useTheme} from "@mui/material";
+import {Link, useLocation} from "react-router-dom";
+import {tokens} from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import SupportAgentOutlinedIcon from "@mui/icons-material/SupportAgentOutlined";
 import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import { AuthContext } from "../../utils/AuthContext";
+import {AuthContext} from "../../utils/AuthContext";
 
-const Item = ({ title, slug, to, icon, selected, setSelected }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  return (
-    <Link to={to} style={{ textDecoration: "none" }}>
-      <MenuItem
-        active={selected === slug}
-        style={{ color: colors.grey[100] }}
-        onClick={() => setSelected(slug)}
-        icon={icon}
-        component={"span"}
-      >
-        <Typography>{title}</Typography>
-      </MenuItem>
-    </Link>
-  );
+const Item = ({title, slug, to, icon, selected, setSelected}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Link to={to} style={{textDecoration: "none"}}>
+            <MenuItem
+                active={selected === slug}
+                style={{color: colors.grey[100]}}
+                onClick={() => setSelected(slug)}
+                icon={icon}
+                component={"span"}
+            >
+                <Typography>{title}</Typography>
+            </MenuItem>
+        </Link>
+    );
 };
 
 const Sidebar = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [currentUser] = useContext(AuthContext);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const location = useLocation();
-  const [selected, setSelected] = useState(
-    location.pathname.split("/")[1] === ""
-      ? "dashboard"
-      : location.pathname.split("/")[1]
-  );
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentUser] = useContext(AuthContext);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const location = useLocation();
+    const [selected, setSelected] = useState(
+        location.pathname.split("/")[1] === ""
+            ? "dashboard"
+            : location.pathname.split("/")[1]
+    );
 
-  return (
-    <Box
-      sx={{
-        ".ps-sidebar-root": {
-          borderRightWidth: "0px",
-          height: "100%",
-        },
-      }}
-    >
-      <ProSidebar
-        collapsed={isCollapsed}
-        rootStyles={{
-          [`.${sidebarClasses.container}`]: {
-            backgroundColor: `${colors.primary[400]} !important`,
-          },
-          [`.${menuClasses.active}`]: {
-            color: `${colors.blueAccent[400]} !important`,
-          },
-          [`.${menuClasses.button}`]: {
-            padding: "5px 35px 5px 20px !important",
-          },
-          [`.${menuClasses.button}:hover`]: {
-            backgroundColor: `${colors.blueAccent[700]} !important`,
-          },
-        }}
-      >
-        <Menu iconShape="square">
-          {/* LOGO AND MENU ICON */}
-          <MenuItem
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{
-              margin: "0 0 20px 0",
-              color: colors.grey[100],
+    return (
+        <Box
+            sx={{
+                ".ps-sidebar-root": {
+                    borderRightWidth: "0px",
+                    height: "100%",
+                },
             }}
-          >
-            {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="space-around"
-                alignItems="center"
-                ml="15px"
-              >
-                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon />
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
+        >
+            <ProSidebar
+                collapsed={isCollapsed}
+                rootStyles={{
+                    [`.${sidebarClasses.container}`]: {
+                        backgroundColor: `${colors.primary[400]} !important`,
+                    },
+                    [`.${menuClasses.active}`]: {
+                        color: `${colors.blueAccent[400]} !important`,
+                    },
+                    [`.${menuClasses.button}`]: {
+                        padding: "5px 35px 5px 20px !important",
+                    },
+                    [`.${menuClasses.button}:hover`]: {
+                        backgroundColor: `${colors.blueAccent[700]} !important`,
+                    },
+                }}
+            >
+                <Menu iconShape="square">
+                    {/* LOGO AND MENU ICON */}
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon/> : undefined}
+                        style={{
+                            margin: "0 0 20px 0",
+                            color: colors.grey[100],
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box
+                                display="flex"
+                                justifyContent="space-around"
+                                alignItems="center"
+                                ml="15px"
+                            >
+                                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                    <MenuOutlinedIcon/>
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
 
-          {/* USER */}
-          {!isCollapsed ? (
-            <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <Avatar
-                  alt="profile-user"
-                  sx={{
-                    width: "100px",
-                    height: "100px",
-                    fontSize: "45px",
-                    bgcolor: colors.greenAccent[300],
-                  }}
-                >
-                  {currentUser.name?.charAt(0).toUpperCase() || ""}
-                  {currentUser.surname?.charAt(0).toUpperCase() || ""}
-                </Avatar>
-              </Box>
-              <Box textAlign="center">
-                <Typography
-                  variant="h2"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 10px 0" }}
-                >
-                  {currentUser?.name || ""} <br /> {currentUser?.surname || ""}
-                </Typography>
-                <Typography variant="h5" color={colors.greenAccent[400]}>
-                  {currentUser?.role}
-                </Typography>
-              </Box>
-            </Box>
-          ) : (
-            <Box>
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <Avatar
-                  alt="profile-user"
-                  sx={{
-                    width: "50px",
-                    height: "50px",
-                    fontSize: "25px",
-                    margin: "5px 0",
-                    bgcolor: colors.greenAccent[300],
-                  }}
-                >
-                  {currentUser.name?.charAt(0).toUpperCase() || ""}
-                  {currentUser.surname?.charAt(0).toUpperCase() || ""}
-                </Avatar>
-              </Box>
-            </Box>
-          )}
+                    {/* USER */}
+                    {!isCollapsed ? (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <Avatar
+                                    alt="profile-user"
+                                    sx={{
+                                        width: "100px",
+                                        height: "100px",
+                                        fontSize: "45px",
+                                        bgcolor: colors.greenAccent[300],
+                                    }}
+                                >
+                                    {currentUser.name?.charAt(0).toUpperCase() || ""}
+                                    {currentUser.surname?.charAt(0).toUpperCase() || ""}
+                                </Avatar>
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{m: "10px 0 10px 0"}}
+                                >
+                                    {currentUser?.name || ""} <br/> {currentUser?.surname || ""}
+                                </Typography>
+                                <Typography variant="h5" color={colors.greenAccent[400]}>
+                                    {currentUser?.role}
+                                </Typography>
+                            </Box>
+                        </Box>
+                    ) : (
+                        <Box>
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <Avatar
+                                    alt="profile-user"
+                                    sx={{
+                                        width: "50px",
+                                        height: "50px",
+                                        fontSize: "25px",
+                                        margin: "5px 0",
+                                        bgcolor: colors.greenAccent[300],
+                                    }}
+                                >
+                                    {currentUser.name?.charAt(0).toUpperCase() || ""}
+                                    {currentUser.surname?.charAt(0).toUpperCase() || ""}
+                                </Avatar>
+                            </Box>
+                        </Box>
+                    )}
 
-          {/* MENU ITEMS */}
-          <Box>
-            <Item
-              title="Dashboard"
-              slug="dashboard"
-              to="/"
-              icon={<HomeOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            {(currentUser.role === "Manager" || currentUser.role === "Expert") && (
-              <Item
-                title="Users"
-                slug="users"
-                to="users"
-                icon={<ContactsOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-            )}
-            <Item
-              title="Products"
-              slug="products"
-              to="/products"
-              icon={<Inventory2OutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            {(currentUser.role === "Manager" || currentUser.role === "Client") && (
-            <Item
-              title={
-                currentUser.role === "Client" ? "My Orders" : "Orders"
-              }
-              slug="orders"
-              to="/orders"
-              icon={<ShoppingCartOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />)}
-            <Item
-              title={
-                currentUser.role === "Client" ? "My Tickets" : "Tickets"
-              }
-              slug="tickets"
-              to="/tickets"
-              icon={<SupportAgentOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-          </Box>
-        </Menu>
-      </ProSidebar>
-    </Box>
-  );
+                    {/* MENU ITEMS */}
+                    <Box>
+                        <Item
+                            title="Dashboard"
+                            slug="dashboard"
+                            to="/"
+                            icon={<HomeOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        {(currentUser.role === "Manager" || currentUser.role === "Expert") && (
+                            <Item
+                                title="Users"
+                                slug="users"
+                                to="users"
+                                icon={<ContactsOutlinedIcon/>}
+                                selected={selected}
+                                setSelected={setSelected}
+                            />
+                        )}
+                        <Item
+                            title="Products"
+                            slug="products"
+                            to="/products"
+                            icon={<Inventory2OutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                        {(currentUser.role === "Manager" || currentUser.role === "Client") && (
+                            <Item
+                                title={
+                                    currentUser.role === "Client" ? "My Orders" : "Orders"
+                                }
+                                slug="orders"
+                                to="/orders"
+                                icon={<ShoppingCartOutlinedIcon/>}
+                                selected={selected}
+                                setSelected={setSelected}
+                            />)}
+                        <Item
+                            title={
+                                currentUser.role === "Client" ? "My Tickets" : "Tickets"
+                            }
+                            slug="tickets"
+                            to="/tickets"
+                            icon={<SupportAgentOutlinedIcon/>}
+                            selected={selected}
+                            setSelected={setSelected}
+                        />
+                    </Box>
+                </Menu>
+            </ProSidebar>
+        </Box>
+    );
 };
 
 export default Sidebar;
