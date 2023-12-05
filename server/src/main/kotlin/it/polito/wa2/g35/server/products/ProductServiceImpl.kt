1966af172027c01fabea8faabd3b56f8aeb0668e
Michele Morgigno <michelemorgigno@gmail.com>
2023-12-05 21:13:58 +0100

    fix: updateProduct

--- server/src/main/kotlin/it/polito/wa2/g35/server/products/ProductServiceImpl.kt
+++ server/src/main/kotlin/it/polito/wa2/g35/server/products/ProductServiceImpl.kt
@@ -1,13 +1,9 @@
 package it.polito.wa2.g35.server.products
 
 import io.micrometer.observation.annotation.Observed
-import it.polito.wa2.g35.server.profiles.ProfileNotFoundException
 import it.polito.wa2.g35.server.profiles.UnauthorizedProfileException
-import it.polito.wa2.g35.server.profiles.customer.Customer
-import it.polito.wa2.g35.server.profiles.customer.toDTO
 import it.polito.wa2.g35.server.security.SecurityConfig
 import it.polito.wa2.g35.server.ticketing.ticket.TicketController
-import it.polito.wa2.g35.server.ticketing.ticket.toTicket
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
@@ -19,6 +15,7 @@ class ProductServiceImpl(
     private val productRepository: ProductRepository
 ) : ProductService {
     private val log: Logger = LoggerFactory.getLogger(TicketController::class.java)
+
     @Observed(
         name = "/products/",
         contextualName = "get-products-request-service"
@@ -27,21 +24,22 @@ class ProductServiceImpl(
         log.info("Get products from repository request successful")
         return productRepository.findAll().map { it.toDTO() }
     }
+
     @Observed(
         name = "/products/{productId}",
         contextualName = "get-product-by-id-request-service"
     )
     override fun getProductById(productId: String): ProductDTO? {
         val product = productRepository.findByIdOrNull(productId)?.toDTO()
-        if(product != null) {
+        if (product != null) {
             log.info("Get product by Id from repository request successful")
             return product
-        }
-        else {
+        } else {
             log.error("Product not found with this product id!")
             throw ProductNotFoundException("Product not found with this product id!")
         }
     }
+
     @Observed(
         name = "/products",
         contextualName = "post-product-request-service"
@@ -49,7 +47,7 @@ class ProductServiceImpl(
     override fun createProduct(product: ProductDTO?): ProductDTO? {
         return if (product != null) {
             val checkIfProductExists = productRepository.findByIdOrNull(product.id)
-            if(checkIfProductExists == null) {
+            if (checkIfProductExists == null) {
                 log.info("Create product request successful (repository)")
                 productRepository.save(Product(product.id, product.name, product.description, product.price, product.quantity, product.warrantyDuration)).toDTO()
             } else {
@@ -60,27 +58,24 @@ class ProductServiceImpl(
             null
         }
     }
-    
+
     @Observed(
         name = "/products/{id}",
         contextualName = "put-product-request-service"
     )
     override fun updateProduct(product: ProductDTO?): ProductDTO? {
-        return if(product != null) {
-            val currentTicket = getProductById(product.id)?.toProduct()
-            println(currentTicket)
-            //val checkIfProductExists = productRepository.findByIdOrNull(product.id)
-            if (currentTicket != null) {
+        return if (product != null) {
+            val currentProduct = getProductById(product.id)?.toProduct()
+            if (currentProduct != null) {
                 val authentication = SecurityContextHolder.getContext().authentication
-                if(authentication.authorities.map { it.authority }[0] == SecurityConfig.MANAGER_ROLE){
-                    if (product.id != authentication.name) {
-                        log.error("Update Product request failed by unauthorized access")
-                        throw UnauthorizedProfileException("You can't access this product!")
-                    }
+                if (authentication.authorities.map { it.authority }[0] == SecurityConfig.MANAGER_ROLE) {
+                    log.info("Update Product request successful (repository)")
+                    productRepository.save(Product(product.id, product.name, product.description,
+                        product.price, product.quantity, product.warrantyDuration)).toDTO()
+                } else {
+                    log.error("Update Product request failed by unauthorized access")
+                    throw UnauthorizedProfileException("You can't access this product!")
                 }
-                log.info("Update Product request successful (repository)")
-                productRepository.save(Product(product.id, product.name, product.description,
-                    product.price,product.quantity, product.warrantyDuration)).toDTO()
             } else {
                 log.error("Product with given id doesn't exists!")
                 throw ProductNotFoundException("Product with given id doesn't exists!")
@@ -98,13 +93,12 @@ class ProductServiceImpl(
     )
     override fun updateProductAvailability(productId: String, quantity: Int): ProductDTO? {
         val product = productRepository.findByIdOrNull(productId)
-        if(product != null) {
+        if (product != null) {
             log.info("Update product availability request successful (repository)")
             product.quantity = product.quantity?.minus(quantity)
             productRepository.save(product)
             return product.toDTO()
-        }
-        else {
+        } else {
             log.error("Product not found with this product id!")
             throw ProductNotFoundException("Product not found with this product id!")
         }
